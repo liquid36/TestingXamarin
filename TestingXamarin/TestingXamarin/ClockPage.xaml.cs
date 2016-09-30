b7e1aeae097c0e2a06c54625162f07d6db0a418a
Mariano Botta <marianoabotta@gmail.com>
2016-09-30 16:41:07 -0300

    Initial commit

---
+++ TestingXamarin/TestingXamarin/ClockPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace TestingXamarin
+{
+	public partial class ClockPage : ContentPage
+	{
+		public ClockPage ()
+		{
+			InitializeComponent ();
+            Device.StartTimer(TimeSpan.FromSeconds(1), OnTimerTick);
+        }
+
+        bool OnTimerTick() {
+            DateTime dt = DateTime.Now;
+            timeLabel.Text = dt.ToString("T");
+            dateLabel.Text = dt.ToString("D");
+            return true;
+        }
+    }
+}
---
+++ TestingXamarin/TestingXamarin/listPage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq; 
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace TestingXamarin
+{
+	public class listPage : ContentPage
+	{
+		public listPage ()
+		{
+            ListView listView = new ListView
+            {
+                RowHeight = 40
+            };
+
+            listView.ItemsSource = new string[]
+            {
+                "Buy pears", "Buy oranges", "Buy mangos", "Buy apples", "Buy bananas"
+            };
+
+            listView.ItemSelected += async (sender, e) => {
+                await DisplayAlert("Tapped!", e.SelectedItem + " was tapped.", "OK");
+                await Navigation.PushAsync(new ClockPage());
+            };
+
+            Content = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.Start,
+                Children = {
+                        new Label {
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            Text = "Welcome to Xamarin Forms!"
+                        },
+                        listView
+
+                    }
+            };
+
+        }
+
+        
+
+        
+	}
+}
